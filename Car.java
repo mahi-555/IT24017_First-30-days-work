f89b679679c1275452c38f8dbaa7a229d1507ee2
mahi-555 <moniramahin555@gmail.com>
2026-04-20 00:50:20 +0600

    Car.java

---
+++ Car.java
@@ -0,0 +1,10 @@
+Public Class Car {
+String color;
+String model;
+int year;
+void displayInfo () {
+System.out.println("car Model: " + model);
+System.out.println("car Color: " + color);
+System.out.println("car Year: "  + year);
+}
+}
